72ea57067d88931d2a8190ae869f1cfd89b4728d
Vern Kofford <cheow3@gmail.com>
2021-11-14 23:26:33 -0700

    Added test for selecting dropdown

--- test/homework_test.exs
+++ test/homework_test.exs
@@ -6,8 +6,13 @@ defmodule HomeworkTest do
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "goes to the internet" do
+  test "select from dropdown" do
     navigate_to "https://the-internet.herokuapp.com/"
-    assert("The Internet" == page_title())
+    link = find_element(:link_text, "Dropdown")
+    click(link)
+
+    find_element(:css, "#dropdown option[value='1']") |> click()
+    sel_ele = find_element(:css, "[selected='selected']")
+    assert visible_text(sel_ele) == "Option 1"
   end
 end
